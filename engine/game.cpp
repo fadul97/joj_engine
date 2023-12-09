f7035d0341d766d023e680056a70f592964d828b
Leonardo Fadul <lsffadul@gmail.com>
2023-12-09 13:48:14 -0300

    Game window and input point to Engine window and input

--- engine/game.cpp
+++ engine/game.cpp
@@ -1,11 +1,18 @@
 #include "game.h"
 
+#include "engine.h"
+
 // Static members
 JojPlatform::Win32Window* JojEngine::Game::window = nullptr;	// Pointer to window
 JojPlatform::Win32Input* JojEngine::Game::input = nullptr;		// Pointer to input
 
 JojEngine::Game::Game()
 {
+	if (!window)
+	{
+		window = JojEngine::Engine::window;
+		input = JojEngine::Engine::input;
+	}
 }
 
 JojEngine::Game::~Game()
