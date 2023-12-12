35f018c542e935784178f3f79c461a2f3caf2de2
Leonardo Fadul <lsffadul@gmail.com>
2023-12-11 22:27:09 -0300

    WIP: DX12Renderer

--- renderer/renderer_dx12.cpp
+++ renderer/renderer_dx12.cpp
@@ -8,15 +8,38 @@
 JojRenderer::DX12Renderer::DX12Renderer()
 {
     window = nullptr;
+    graphics = nullptr;
 
+    // Configuration
+    backbuffer_count = 2;	// Double buffering
+    vsync = false;			// No vertical sync
+
+    // Background color
+    bg_color[0] = 0.0f;		// Red
+    bg_color[1] = 0.0f;		// Green
+    bg_color[2] = 0.0f;		// Blue
+    bg_color[3] = 0.0f;		// Alpha (0 = transparent, 1 = solid)
+
+    // Graphics Infrastructure
     device = nullptr;
     factory = nullptr;
-    swap_chain = nullptr;
-    
+    swapchain = nullptr;
+    backbuffer_index = 0;   // 0 is the first
+
+    // Pipeline
+    render_targets = nullptr;
+    depth_stencil = nullptr;
+    render_target_heap = nullptr;
+    depth_stencil_heap = nullptr;
+    rt_descriptor_size = 0;
+    viewport = { 0 };
+    scissor_rect = { 0 };
+
     command_queue = nullptr;
     command_list = nullptr;
     command_list_alloc = nullptr;
 
+    // CPU/GPU Synchronization
     fence = nullptr;
     current_fence = 0;
 }
@@ -24,25 +47,6 @@ JojRenderer::DX12Renderer::DX12Renderer()
 /* ATTENTION: Call DX12Renderer destructor before DX12Graphics destructor */
 JojRenderer::DX12Renderer::~DX12Renderer()
 {
-	// Wait for GPU to finish queued commands
-	wait_command_queue();
-
-	// Release fence
-	if (fence)
-		fence->Release();
-
-	// Release command list
-	if (command_list)
-		command_list->Release();
-
-	// Release command allocator
-	if (command_list_alloc)
-		command_list_alloc->Release();
-
-	// Release command queue
-	if (command_queue)
-		command_queue->Release();
-
 	// Release graphics device
 	if (device)
 		device->Release();
@@ -56,39 +60,35 @@ b8 JojRenderer::DX12Renderer::init(JojPlatform::Win32Window* window, JojGraphics
 {
     // Set window
     this->window = window;
+    this->graphics = graphics;
 
     // Get objects created from graphics
+    backbuffer_count = graphics->get_backbuffer_count();
+    vsync = graphics->get_vsync();
     device = graphics->get_device();
 	factory = graphics->get_factory();
+    swapchain = graphics->get_swapchain();
+    command_queue = graphics->get_command_queue();
+    command_list = graphics->get_command_list();
+    command_list_alloc = graphics->get_command_list_alloc();
+    render_targets = graphics->get_render_targets();
+    depth_stencil = graphics->get_depth_stencil();
+    render_target_heap = graphics->get_render_target_heap();
+    depth_stencil_heap = graphics->get_depth_stencil_heap();
+    viewport = graphics->get_viewport();
+    scissor_rect = graphics->get_scissor_rect();
 
-	// ---------------------------------------------------
-	// Create queue, list and command allocator
-	// ---------------------------------------------------
+    // Background color of the backbuffer = window background color
+    COLORREF color = window->get_color();
 
-	// Create GPU command queue
-	D3D12_COMMAND_QUEUE_DESC queue_desc = {};
-	queue_desc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
-	queue_desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
-	ThrowIfFailed(device->CreateCommandQueue(&queue_desc, IID_PPV_ARGS(&command_queue)));
-
-	// Create command allocator
-	ThrowIfFailed(device->CreateCommandAllocator(
-		D3D12_COMMAND_LIST_TYPE_DIRECT,
-		IID_PPV_ARGS(&command_list_alloc)));
-
-	// Create command list
-	ThrowIfFailed(device->CreateCommandList(
-		0,										// Using only one GPU
-		D3D12_COMMAND_LIST_TYPE_DIRECT,			// Does not inherit state on the GPU
-		command_list_alloc,						// Command allocator
-		nullptr,								// Pipeline initial state
-		IID_PPV_ARGS(&command_list)));			// Command list object
-
-	// ---------------------------------------------------
-	// Create fence to synchronize CPU/GPU
-	// ---------------------------------------------------
-
-	ThrowIfFailed(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+    bg_color[0] = GetRValue(color) / 255.0f;	// Red
+    bg_color[1] = GetGValue(color) / 255.0f;	// Green
+    bg_color[2] = GetBValue(color) / 255.0f;	// Blue
+    bg_color[3] = 1.0f;							// Alpha (1 = solid)
+    
+    rt_descriptor_size = graphics->get_rt_descriptor_size();
+    fence = graphics->get_fence();
+    current_fence = graphics->get_current_fence();
 
     return true;
 }
@@ -98,48 +98,99 @@ void JojRenderer::DX12Renderer::draw()
 
 }
 
-void JojRenderer::DX12Renderer::clear()
+void JojRenderer::DX12Renderer::present()
 {
+    // indica que o backbuffer será usado para apresentação
+    D3D12_RESOURCE_BARRIER barrier = {};
+    barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+    barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+    barrier.Transition.pResource = render_targets[backbuffer_index];
+    barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+    barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
+    barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+    command_list->ResourceBarrier(1, &barrier);
 
+    // submete a lista de comandos para execução na GPU
+    submit_commands();
+
+    // Show frame and swap front/back buffers
+    swapchain->Present(vsync, 0);
+    backbuffer_index = (backbuffer_index + 1) % backbuffer_count;
+}
+
+void JojRenderer::DX12Renderer::clear(ID3D12PipelineState* pso)
+{
+    /* Reuses the memory associated with the command list
+       The list of commands should have finished running on the GPU */
+    command_list_alloc->Reset();
+
+    /* A list of commands can be reinitialized after added to 
+     GPU command queue (via ExecuteCommandList) */
+    command_list->Reset(command_list_alloc, pso);
+
+    // TODO: comment specifications on barrier
+    // Indicate that the backbuffer will be used as a render target
+    D3D12_RESOURCE_BARRIER barrier = {};
+    barrier.Type = D3D12_RESOURCE_BARRIER_TYPE_TRANSITION;
+    barrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
+    barrier.Transition.pResource = render_targets[backbuffer_index];
+    barrier.Transition.StateBefore = D3D12_RESOURCE_STATE_PRESENT;
+    barrier.Transition.StateAfter = D3D12_RESOURCE_STATE_RENDER_TARGET;
+    barrier.Transition.Subresource = D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES;
+    command_list->ResourceBarrier(1, &barrier);
+
+    // Adjust viewport and clipping rectangles
+    command_list->RSSetViewports(1, &viewport);
+    command_list->RSSetScissorRects(1, &scissor_rect);
+
+    // Clear backbuffer and depth/stencil buffer
+    D3D12_CPU_DESCRIPTOR_HANDLE ds_handle = depth_stencil_heap->GetCPUDescriptorHandleForHeapStart();
+    D3D12_CPU_DESCRIPTOR_HANDLE rt_handle = render_target_heap->GetCPUDescriptorHandleForHeapStart();
+    rt_handle.ptr += SIZE_T(backbuffer_index) * SIZE_T(rt_descriptor_size);
+    command_list->ClearRenderTargetView(ds_handle, bg_color, 0, nullptr);
+    command_list->ClearDepthStencilView(rt_handle, D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+
+    // Specify which buffers will be used in rendering
+    command_list->OMSetRenderTargets(1, &rt_handle, true, &ds_handle);
 }
 
 b8 JojRenderer::DX12Renderer::wait_command_queue()
 {
-	// Advance fence value to mark new commands from that point
-	current_fence++;
+    // Advance fence value to mark new commands from that point
+    current_fence++;
 
-	// Add an instruction to the command queue to insert a new fence
-	// GPU will finish all ongoing commands before processing this signal
-	if (FAILED(command_queue->Signal(fence, current_fence)))
-		return false;
+    // Add an instruction to the command queue to insert a new fence
+    // GPU will finish all ongoing commands before processing this signal
+    if (FAILED(command_queue->Signal(fence, current_fence)))
+        return false;
 
-	// Wait for GPU to complete all previous commands
-	if (fence->GetCompletedValue() < current_fence)
-	{
-		HANDLE event_handle = CreateEventEx(NULL, NULL, NULL, EVENT_ALL_ACCESS);
+    // Wait for GPU to complete all previous commands
+    if (fence->GetCompletedValue() < current_fence)
+    {
+        HANDLE event_handle = CreateEventEx(NULL, NULL, NULL, EVENT_ALL_ACCESS);
 
-		if (event_handle)
-		{
-			// Trigger event when GPU reaches current fence
-			if (FAILED(fence->SetEventOnCompletion(current_fence, event_handle)))
-				return false;
+        if (event_handle)
+        {
+            // Trigger event when GPU reaches current fence
+            if (FAILED(fence->SetEventOnCompletion(current_fence, event_handle)))
+                return false;
 
-			// Wait until the event is triggered
-			WaitForSingleObject(event_handle, INFINITE);
-			CloseHandle(event_handle);
-		}
-	}
+            // Wait until the event is triggered
+            WaitForSingleObject(event_handle, INFINITE);
+            CloseHandle(event_handle);
+        }
+    }
 
-	return true;
+    return true;
 }
 
 void JojRenderer::DX12Renderer::submit_commands()
 {
-	// submits the commands recorded in the list for execution on the GPU
-	command_list->Close();
-	ID3D12CommandList* cmds_lists[] = { command_list };
-	command_queue->ExecuteCommandLists(_countof(cmds_lists), cmds_lists);
+    // submits the commands recorded in the list for execution on the GPU
+    command_list->Close();
+    ID3D12CommandList* cmds_lists[] = { command_list };
+    command_queue->ExecuteCommandLists(_countof(cmds_lists), cmds_lists);
 
-	// Wait until GPU completes executing the commands
-	wait_command_queue();
+    // Wait until GPU completes executing the commands
+    wait_command_queue();
 }
--- renderer/renderer_dx12.h
+++ renderer/renderer_dx12.h
@@ -20,27 +20,42 @@ namespace JojRenderer
 	{
 	public:
 		DX12Renderer();
-
-		/* ATTENTION: Call DX12Renderer destructor before DX12Graphics destructor */
 		~DX12Renderer();
 
 		b8 init(JojPlatform::Win32Window* window, JojGraphics::DX12Graphics* graphics);	// Initialize renderer
 		void draw();																	// Draw
-		void clear();																	// Clear backbuffer for next frame
+		void present();																	// Present
+		void clear(ID3D12PipelineState* pso);											// Clear backbuffer for next frame
 
 	private:
 		JojPlatform::Win32Window* window;
+		JojGraphics::DX12Graphics* graphics;
 
-		// Pipeline
+		// Configuration
+		u32 backbuffer_count;							// Number of buffers on the swap chain (double, triple, etc.)
+		b8 vsync;										// Vertical sync 
+		f32 bg_color[4];								// Backbuffer background color
+
+		// Graphics Infrastructure
 		ID3D12Device* device;							// Graphics device
 		IDXGIFactory6* factory;							// Main DXGI interface
-		IDXGISwapChain* swap_chain;						// Swap chain
+		IDXGISwapChain* swapchain;						// Swap chain
+		u32 backbuffer_index;							// Current backbuffer index
+
+		// Pipeline
+		ID3D12Resource** render_targets;				// Buffers for rendering (front and back)
+		ID3D12Resource* depth_stencil;					// Depth & Stencil Buffer            
+		ID3D12DescriptorHeap* render_target_heap;       // Descriptor heap for render targets
+		ID3D12DescriptorHeap* depth_stencil_heap;       // Descriptor heap for Depth Stencil
+		u32 rt_descriptor_size;							// Size of each Render Target descriptor
+		D3D12_VIEWPORT viewport;						// Viewport
+		D3D12_RECT scissor_rect;						// Scissor rect
 
 		ID3D12CommandQueue* command_queue;              // GPU command queue
 		ID3D12GraphicsCommandList* command_list;        // List of commands to submit to GPU
 		ID3D12CommandAllocator* command_list_alloc;     // Memory used by the command list
 
-		// Synchronization                         
+		// CPU/GPU Synchronization
 		ID3D12Fence* fence;								// Fence to synchronize CPU/GPU
 		u64 current_fence;								// Fence counter
 
