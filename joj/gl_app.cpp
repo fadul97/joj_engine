aea349a5428e0cc4cdcb31964c8d287e5b401b81
Leonardo Fadul <lsffadul@gmail.com>
2024-01-03 23:42:05 -0300

    Fixed glBufferData call, drawing geometries now

--- joj/gl_app.cpp
+++ joj/gl_app.cpp
@@ -6,22 +6,21 @@
 #include "logger.h"
 #include <iostream>
 #include "opengl/shader.h"
+#include <fstream>
 
 Mat4 perspective;
 Mat4 ortho;
 u32 uniform;
 
-void GLApp::init()
+void GLApp::build_buffers()
 {
-    geo = JojRenderer::Cube(0.1f, 0.1f, 0.1f);
-    //geo = JojRenderer::GeoSphere(0.2f, 3);
-
-    float vertices[] = {
-         0.5f,  0.5f, -1.0f,  // top right
-         0.5f, -0.5f, -1.0f,  // bottom right
-        -0.5f, -0.5f, -1.0f,  // bottom left
-        -0.5f,  0.5f, -1.0f   // top left 
+    DirectX::XMFLOAT3 vertices[] = {
+        DirectX::XMFLOAT3{ +0.5f, +0.5f, +0.0f },  // top right
+        DirectX::XMFLOAT3{ +0.5f, -0.5f, +0.0f },  // bottom right
+        DirectX::XMFLOAT3{ -0.5f, -0.5f, +0.0f },  // bottom left
+        DirectX::XMFLOAT3{ -0.5f, +0.5f, +0.0f }   // top left 
     };
+
     unsigned int indices[] = {  // note that we start from 0!
         0, 1, 3,  // first Triangle
         1, 2, 3   // second Triangle
@@ -37,31 +36,54 @@ void GLApp::init()
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
     // Fill the vbo with the vertex data
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, (geo.get_vertex_count() * sizeof(JojRenderer::Vertex)), geo.get_vertex_data(), GL_STATIC_DRAW);
 
     // Fill the ebo with the vertex data
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, geo.get_index_count() * sizeof(u32), geo.get_index_data(), GL_STATIC_DRAW);
 
     // Specify the layout of the vertex data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(f32), (GLvoid*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 7 * sizeof(f32), (GLvoid*)0);
     glEnableVertexAttribArray(0);
 
     // Unbind the vbo and the vao
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+}
 
-    shader.compile_shaders(geo_shader, fragmentShaderSource);
+void GLApp::init()
+{
+    // Geometries
+    //geo = JojRenderer::Cube(1.0f, 1.0f, 1.0f);
+    //geo = JojRenderer::Cylinder(1.0f, 0.5f, 0.7f, 20, 10);
+    //geo = JojRenderer::Sphere(1.0f, 40, 40);
+    //geo = JojRenderer::GeoSphere(1.0f, 3);
+    //geo = JojRenderer::Grid(100.0f, 20.0f, 20, 20);
+    geo = JojRenderer::Quad(3.0f, 1.0f);
+
+    build_buffers();
+
+    //shader = JojRenderer::Shader{ vshader_path , vfrag_path };
+    shader.compile_shaders(vertexShaderSource, fragmentShaderSource);
     shader.use();
 
-    perspective = mat4_perspective(90.0f, JojEngine::Engine::pm->get_window()->get_aspect_ratio(), 0.1f, 100.0f);
+    std::ifstream file("../shaders/frag.glsl");
+
+    if (!file) {
+        std::cerr << "Unable to open file frag.glsl";
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        std::cout << line << '\n';
+    }
+
+    file.close();
+
+    perspective = mat4_perspective(90.0f, JojEngine::Engine::pm->get_window()->get_aspect_ratio(), 0.1f, 10.0f);
     ortho = mat4_orthographic(-2.0f, 2.0f, -2.0f, 2.0f, 0.0f, 100.0f);
+}
 
-    Mat4 scale = mat4_scale(mat4_identity(), vec3_create(0.8f, 0.8f, 0.8f));
-    Mat4 t = mat4_mul(perspective, scale);
-
-    shader.set_mat4(std::string{ "transform" }, t);
-}   
 
 b8 is_ortho = false;
 f32 angle = 0.0f;
@@ -101,12 +123,14 @@ void GLApp::update()
         if (is_ortho)
         {
             Mat4 t = mat4_mul(ortho, trans);
-            shader.set_mat4(std::string{ "transform" }, t);
+            shader.set_mat4("transform", t);
+            mat4_print(t);
         }
         else
         {
             Mat4 t = mat4_mul(trans, perspective);
-            shader.set_mat4(std::string{ "transform" }, t);
+            shader.set_mat4("transform", t);
+            mat4_print(t);
         }
     }
 }
@@ -115,11 +139,11 @@ void GLApp::draw()
 {
     
     glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.3f, 1.0f);
 
     shader.use();
     glBindVertexArray(vao);         // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, geo.get_index_count(), GL_UNSIGNED_INT, 0);
     
 
     JojEngine::Engine::pm->swap_buffers();
--- joj/gl_app.h
+++ joj/gl_app.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "game.h"
-#include "geometry.h"
+#include "opengl/geometry.h"
 #include "fmath.h"
 #include "opengl/shader.h"
+#include "opengl/quad.h"
+#include "geometry.h"
+#include "DirectXMath.h"
 
 class GLApp : public JojEngine::Game
 {
@@ -14,6 +17,8 @@ public:
 	void shutdown();
 
 private:
+	void build_buffers();
+
 	// Define the vertex shader source code
 	const char* vertexShaderSource = "#version 330 core\n"
 		"layout (location = 0) in vec3 position;\n"
@@ -27,7 +32,7 @@ private:
 		"out vec4 color;\n"
 		"void main()\n"
 		"{\n"
-		"color = vec4(1.0f, 0.2f, 0.6f, 1.0f);\n"
+		"color = vec4(1.0f, 0.4f, 0.6f, 1.0f);\n"
 		"}\n\0";
 
 	const char* geo_shader = "#version 330 core\n"
@@ -38,24 +43,50 @@ private:
 		"	gl_Position = transform * vec4(pos, 1.0);\n"
 		"}\0";
 
+	const char* r_vert = "#version 330 core\n"
+		"layout (location = 0) in vec3 pos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(pos, 1.0);\n"
+		"}\0";
+
+	const char* r_frag = "#version 330 core\n"
+		"out vec4 color;\n"
+		"void main()\n"
+		"{\n"
+		"	vec2 uv = gl_FragCoord.xy; // position of the fragment\n"
+		"	vec2 pos = 2.0 * uv - 1.0; // convert uv to [-1, 1] range\n"
+		"	float curve = 0.5; // amount of curve\n"
+		"	vec2 d = abs(pos)-vec2(0.3, 0.3);\n"
+		"	float dist = length(max(d,0.0)) - curve; // distance from center\n"
+		"	if (dist > 0.3)\n"
+		"	{\n"
+		"		discard; // discard fragments outside the radius\n"
+		"	}\n"
+		"	else\n"
+		"	{\n"
+		"		color = vec4(0.0f, 0.0f, 1.0f, 1.0f); // color of the quad\n"
+		"	}\n"
+		"}\n\0";
+
+	const char* vshader_path = "../shaders/vert.glsl";
+	const char* vfrag_path = "../shaders/frag.glsl";
+
 	u32 vbo = 0;
 	u32 vao = 0;
 	u32 ebo = 0;
 	i32 shader_program = 0;
-	JojRenderer::Shader shader = JojRenderer::Shader{};
+	JojRenderer::Shader shader;
 
-	JojRenderer::Cube geo = {};
+	//JojRenderer::Cube geo = {};
+	//JojRenderer::Cylinder geo = {};
+	//JojRenderer::Sphere geo = {};
 	//JojRenderer::GeoSphere geo = {};
+	//JojRenderer::Grid geo = {};
+	JojRenderer::Quad geo = {};
 
-	// Camera settings
-	Mat4 World = {};
-	Mat4 View = {};
-	Mat4 Proj = {};
+	MyQuad q = MyQuad{ 1.0f, 1.0f };
 
-	f32 theta = 0;
-	f32 phi = 0;
-	f32 radius = 0;
-
-	f32 last_xmouse = 0;
-	f32 last_ymouse = 0;
+	Mat4 perspective ={};
+	Mat4 ortho ={};
 };
